f13768274740afef1a08f353a793000c7b02a394
Yaroslav <doctor355@yahoo.com>
2019-04-05 12:32:43 +0300

    Create item components

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-
+import {Row, Col} from 'react-bootstrap';
+import Collapsible from 'react-collapsible';
 
 class Books extends Component {
     render() {
+        let bookItems;
+        if(this.props.books){
+            bookItems = this.props.books.map(book => {
+                let id = book.id;
+                let title = book.volumeInfo.title;
+                return (
+                    <Collapsible key={id} trigger={title}>
+                        <p>This is the collapsible content. It can be any element or React component you like.</p>
+                        <p>It can even be another Collapsible component. Check out the next section!</p>
+                    </Collapsible>
+                    
+                )
+            })
+        }
         return (
-            <div>BOOKS</div>
+            <div>{bookItems}</div>
+
         );
     }
 }
